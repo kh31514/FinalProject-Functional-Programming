417d7519eb6f08503a757befac1bbabb64b2f194
Henry Koontz <htk24@cornell.edu>
2023-03-20 23:31:06 -0400

    Added some authorization code to spotify.ml

---
+++ src/app.ml
@@ -0,0 +1,1 @@
+let client_id = 
--- src/spotify.ml
+++ src/spotify.ml
@@ -1,3 +1,13 @@
+let redirect_URl = "git@github.coecis.cornell.edu:kah294/cs3110-final-project.git"
+let client_id = "fac6c1e9d41148518b698b35d29fb9dd"
+let client_secret = "66510a496dbe43859f5de34ae6a9be99"
+
+let const = "https://accounts.spotity.com/authorize"
+let scope = "&scope=user-read-private"
+
+let request_authorization = const ^ "?client_id" ^ client_id ^ "&response_type=code&redirt_uri=" ^ redirect_URl ^ "&show_dialog=true" ^ scope;
+
+let token = "";
 
 TODO: figure out import issues (check dune file?)
 open spotify-web-api
